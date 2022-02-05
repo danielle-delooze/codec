64c30a2706068fa63a8d7336f844e36e49f85beb
Danielle DeLooze <danielle.delooze@gmail.com>
2022-02-04 19:48:29 -0500

    move AES functions into their own files

---
+++ aes.cpp
@@ -0,0 +1,27 @@
+#include <string>
+
+std::string addRoundKey(std::string input) {
+  return input;
+}
+
+std::string subBytes(std::string input) {
+  return input;
+}
+
+std::string shiftRows(std::string input) {
+  return input;
+}
+
+std::string mixColumns(std::string input) {
+  return input;
+}
+
+std::string AESEncryption(std::string input) {
+  /*
+    Advanced Encryption Standard
+    Symmetric key symmetric block cipher
+    128-bit data, 128/192/256-bit keys
+  */
+
+  return input;
+}
--- codec.cpp
+++ codec.cpp
@@ -1,33 +1,7 @@
 #include <iostream>
 #include <string>
 
-std::string addRoundKey(std::string input) {
-
-}
-
-std::string subBytes(std::string input) {
-
-}
-
-std::string shiftRows(std::string input) {
-
-}
-
-std::string mixColumns(std::string input) {
-
-}
-
-std::string AESEncryption(std::string input) {
-  /*
-    Advanced Encryption Standard
-    Symmetric key symmetric block cipher
-    128-bit data, 128/192/256-bit keys
-  */
-
-  return input;
-}
-
-
+std::string AESEncryption(std::string input);
 
 int main() {
   std::string input;
